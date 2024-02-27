ddfea5bbf40ac4f9be39128f5da6f83c613c4170
spetev1 <92394460+spetev1@users.noreply.github.com>
2024-02-27 15:43:09 +0200

    Add files via upload

---
+++ PS_45_Stanislav/Welcome/Program.cs
@@ -0,0 +1,12 @@
+﻿using Welcome.Model;
+
+namespace Welcome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User();
+        }
+    }
+}
---
+++ PS_45_Stanislav/Welcome/ViewModel/UserViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Welcome.Model;
+using Welcome.Others;
+
+namespace Welcome.ViewModel
+{
+    internal class UserViewModel
+    {
+        private User _user;
+
+        public string Name
+        {
+            get { return _user.Name; }
+            set { _user.Name = value; }
+        }
+        public string Password
+        {
+            get { return _user.Password; }
+            set { _user.Password = value; }
+        }
+        public UserRolesEnum Role
+        {
+            get { return _user.Role; }
+            set { _user.Role = value; }
+        }
+    }
+}
