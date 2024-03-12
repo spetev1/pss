7f2b982c5d4ca0dc72458e99f4e184a6d4c4e1bc
spetev1 <92394460+spetev1@users.noreply.github.com>
2024-03-12 12:14:26 +0200

    Add files via upload

---
+++ PS_45_Stanislav-12-03/Welcome/Program.cs
@@ -0,0 +1,18 @@
+﻿using Welcome.Model;
+using Welcome.View;
+using Welcome.ViewModel;
+
+namespace Welcome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User("Stanislav","12345",Others.UserRolesEnum.ADMIN,"sp@gmail.com","1111");
+            UserViewModel userviewm1 = new UserViewModel(user1);
+            UserView userv1 = new UserView(userviewm1);
+            userv1.Display();
+
+        }
+    }
+}
---
+++ PS_45_Stanislav-12-03/Welcome/View/UserView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Welcome.ViewModel;
+
+namespace Welcome.View
+{
+    internal class UserView
+    {
+        private UserViewModel _viewModel;
+
+        public UserView(UserViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Welcome");
+            Console.WriteLine(_viewModel.Name);
+            Console.WriteLine(_viewModel.Role);
+            Console.WriteLine(_viewModel.facNumber);
+            Console.WriteLine(_viewModel.email);
+
+        }
+    }
+}
